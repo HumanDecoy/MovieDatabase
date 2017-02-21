36fee090db1cb4abf066ccf7019b519054f45ddb
Oscar Gren <grenen89at-signgmail.com>
2017-02-21 12:51:16 +0100

    save

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
-const MovieDatabase = [
-{
-	title: "Example Film",
-	year: 1915,
-	genres: ["Horror", "ExampleStyle"],
-	ratings:[1,5,7,4,2,7,8,9,3]
-},
-{
-	
-},
-{},
-{},
-{},
-];
+const MovieDatabase = {
+	movies : [
+	{
+		title: "Example Film",
+		year: 1915,
+		genres: ["Horror", "ExampleStyle"],
+		ratings:[1,5,7,4,2,7,8,9,3]
+	},
+	{},
+	{},
+	{},
+	{},
+	]
+};
+
+
+// Module pattern med ES6 Lägg i moviedatabase i denna ! 
+const allCountries = (() => {let countries = countryDatabase.countries;
+	return {
+		Getcountries: () => {return countries;}
+	};
+})();
+
+
+
+
+/*   EXEMPEL PÅ ATT TA UT INFORMATION FRÅN MOVIE DATABASE
+
+const onlyEU = allCountries.Getcountries().filter((x) => {
+    if (x.continent === "Europe"){
+        return x;}
+    });
+
+console.log(onlyEU);
+*/
