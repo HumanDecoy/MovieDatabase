77ddda228acdcc435480b0fa10e5165d690e3d54
Oscar Gren <grenen89at-signgmail.com>
2017-02-21 13:52:15 +0100

    Påbörjat arbetet

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
-const MovieDatabase = {
-	movies : [
+// Module pattern med ES6 Lägg i moviedatabase i denna ! 
+
+
+const allMovies = (() => {let MovieDatabase = {
+	films : [
 	{
-		title: "Example Film",
-		year: 1915,
-		genres: ["Horror", "ExampleStyle"],
+		title: "Finding Nemo",
+		year: 2003,
+		genres: ["Horror", "Thriller", "2-spooky-4-me"],
 		ratings:[1,5,7,4,2,7,8,9,3]
 	},
-	{},
-	{},
-	{},
-	{},
+	{
+		title: "Waterworld",
+		year: 1995 ,
+		genres: ["Action"],
+		ratings: [8,7,6,9,8,7,10]
+	},
+	{
+		title: "Stargate",
+		year: 1994 ,
+		genres: ["Action","Adventure","Sci-Fi"],
+		ratings: [10,10,10,9,8,10,8]
+	},
+	{
+		title: "Arrival",
+		year: 2016 ,
+		genres: ["Action","Sci-Fi"],
+		ratings: [5,7,3,7,9]
+	},
+	{
+		title: "The Brothers Lionheart",
+		year: 1977 ,
+		genres: ["Action", "Adventure"],
+		ratings: [5,7,8,2,4,7,9]
+	},
 	]
 };
 
-
-// Module pattern med ES6 Lägg i moviedatabase i denna ! 
-const allCountries = (() => {let countries = countryDatabase.countries;
-	return {
-		Getcountries: () => {return countries;}
-	};
+return {
+	Getmovies: () => {return MovieDatabase.films;}
+};
 })();
 
 
@@ -32,4 +52,68 @@ const onlyEU = allCountries.Getcountries().filter((x) => {
     });
 
 console.log(onlyEU);
+*/
+console.log (allMovies.Getmovies());
+
+
+
+
+
+/* AVAKTA MED DENNA
+const onlyHorror = allMovies.Getmovies().filter((x) => {
+	
+
+	if (x.genres === "Action"){
+		return x;}
+});
+console.log(onlyHorror);
+*/
+
+
+/*
+const onlyEU = allCountries.Getcountries().filter((x) => {
+    if (x.continent === "Europe"){
+        return x;}
+    });
+    */
+
+
+
+
+
+    /*  DATABASEN I OBJEKT ENDAST.
+const MovieDatabase = {
+	movies : [
+	{
+		title: "Finding Nemo",
+		year: 2003,
+		genres: ["Horror", "Thriller", "2-spooky-4-me"],
+		ratings:[1,5,7,4,2,7,8,9,3]
+	},
+	{
+		title: "Waterworld",
+		year: 1995 ,
+		genres: ["Action"],
+		ratings: [8,7,6,9,8,7,10]
+	},
+	{
+		title: "Stargate",
+		year: 1994 ,
+		genres: ["Action","Adventure","Sci-Fi"],
+		ratings: [10,10,10,9,8,10,8]
+	},
+	{
+		title: "Arrival",
+		year: 2016 ,
+		genres: ["Action","Sci-Fi"],
+		ratings: [5,7,3,7,9]
+	},
+	{
+		title: "The Brothers Lionheart",
+		year: 1977 ,
+		genres: ["Action", "Adventure"],
+		ratings: [5,7,8,2,4,7,9]
+	},
+	]
+};
 */
